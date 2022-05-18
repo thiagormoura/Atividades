3dfaead52a08d28683f55779ea1981ad15cc6f08
João Thiago <102044034+Thiaguin001@users.noreply.github.com>
2022-05-18 11:58:12 +0000

    Lista1.cs

---
+++ Lista1/1.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite seu nome");
+   string nome =Console.ReadLine();
+    Console.WriteLine("Seja bem vindo ao C#,"+nome);
+    
+  }}
---
+++ Lista1/2.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Digite sua matricula:");
+   string m =Console.ReadLine();
+    double m1=double.Parse(m);
+    Console.WriteLine("Sua matricula é:"+m1);
+    
+  }}
